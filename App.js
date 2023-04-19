c028a82e3ba55d6bef643643f1a614030cd6a886
smarinv16 <kevindaza13@gmail.com>
2023-04-19 09:01:18 -0500

    agregando nuevos archivos

--- App.js
+++ App.js
@@ -2,44 +2,118 @@
 
 import * as React from 'react';
 import detallesScreen from './screens/detallesScreen.js'
-import { View, Text } from 'react-native';
+import { MaterialIcons } from "@expo/vector-icons";
+import { View, Text, Alert } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Button } from 'react-native-web';
-import { userState } from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { TextInput, Button } from 'react-native-paper';
+import { useState } from 'react'
  
+let users = [{ username: 'Papablo', name: 'Pablo', password: 123456, rol: '1'
+}, {
+  username: 'jairito', name: 'Jairo ', password: 'Yoyo452', rol: '2'
+},
+ {
+  username: 'Pedrolo', name: 'Pedro ', password: 'Lopez45', rol: '3'
+},
+]
 
+
+// Se instancia la libreria bottom Stack Navigator
+
+const Stack = createNativeStackNavigator();
 
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Inicio" component={Inicio} />
-        <Stack.Screen name="Detalles" component={detallesScreen} />
+        <Stack.Screen name="Home" component={HomeTab} options={{title: 'Sistema de pruebas'}} />
+        <Stack.Screen name="Detalles" component={detallesScreen} options={{title: 'Sistema de pruebas'}}  />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-function Inicio({navigation}) {
+function LoginScreen({navigation, route}) {
 
-  let usuarios = [{nombre: "Juan Jaramillo", usuario: "Lasalsa", password: 12345}, {nombre: "Alfredo ", usuario: "Alfrinoo", password: 45678}, {nombre: "Simon Pai", usuario: "ElSaimon", password: 741258}]
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Pantalla de inicio</Text>
-      
-      <Button
-        title="Detalles "
-        onPress={() => navigation.navigate('Detalles', {nombre: "Juan Jaramillo", usuario: "Lasalsa"})}
-      />
+          <Text> Inicio de Sesión  </Text>
+          <TextInput label = 'usuario'
+            mode='outlined'
+            onChangeText={username => setUsername(username)}
+            value= {username}
+            style={{margin: 20}}
+          />
+          <TextInput label = 'password'
+            secureTextEntry
+            mode='outlined'
+            onChangeText={password => setPassword(password)}
+            value= {password}
+            style ={{marginTop: 10, marginBottom: 30}}
+          />
+            <Button icon="account" mode="contained" onPress={() => {
+              let findUser = users.find(user => user.username == username && user.password == password)
+
+              if(findUser != undefined){
+              navigation.navigate('Detalles', {name: findUser.name, username:findUser.username})
+              } else {
+                  alert("INVALIDO")
+              }
+            }}
+            
+            >
+            Ingresar
+          </Button>
+
 
     </View>
   );
 }
 
+function ChatScreen() {
 
-const Stack = createNativeStackNavigator();
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text> CHAT  </Text>
+
+    </View>
+  );
+}
+
+// Se instancia la libreria bottom navigator
+
+const Tab = createBottomTabNavigator();
+
+
+function HomeTab() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: '#0FA3B1',
+        tabBarInactiveTintColor: 'black',
+        tabBarActiveBackgroundColor:'#B5E2FA',
+        tabBarStyle: {
+        backgroundColor: 'white',
+        borderTopColor: 'black'
+        },
+      }}
+    >
+      <Tab.Screen name='Home' component={detallesScreen} />
+      <Tab.Screen name='Login' component={LoginScreen}/>
+      <Tab.Screen name='Chat' component={ChatScreen}/>
+
+    </Tab.Navigator>
+  );
+}
+
+
 
  
--- screens/detallesScreen.js
+++ screens/detallesScreen.js
@@ -1,23 +1,13 @@
 import { View, Text, TextInput } from 'react-native';
+import { userState } from 'react'
 // import { NavigationContainer } from '@react-navigation/native';
 // import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Button } from 'react-native-web';
 
-export default function detallesScreen({navigation, route}){
+export default function DetallesScreen({navigation} ){
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Pantalla de detalles </Text>
-         <Text> Nombre completo: {route.params.nombre}, Usuario: {route.params.usuario}, Contraseña: {route.params.password} </Text> 
-          <TextInput value ={route.params.nombre} 
-          />
-          
-          <TextInput value ={route.params.usuario} 
-          />
-
-        <Button
-        title="ir al inicio "
-        onPress={() => navigation.navigate('Inicio')}
-      />
+      <Text> HOME  </Text>
       </View>
     );
   
